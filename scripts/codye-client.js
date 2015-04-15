ea05c3eb61b6264479cb526cfdfaa493e7f0d0f2
Simon O'Hara <simonpohara@gmail.com>
2015-04-15 22:01:30 +0100

    Refactored the way the style rules are generated.

--- scripts/codye-client.js
+++ scripts/codye-client.js
@@ -3,31 +3,31 @@
 
   CodyeClient.controller('HighlightController', ['$scope', function($scope) {
     var groups = [
-      { name: "comment", color: "black" },
-      { name: "string", color: "black" },
-      { name: "class-name", color: "black" },
-      { name: "regex", color: "black" },
-      { name: "keyword", color: "black" },
-      { name: "boolean", color: "black" },
-      { name: "function", color: "black" },
-      { name: "number", color: "black" },
-      { name: "operator", color: "black" },
-      { name: "ignore", color: "black" },
-      { name: "punctuation", color: "black" }
+      { name: 'comment', color: 'black' },
+      { name: 'string', color: 'black' },
+      { name: 'class-name', color: 'black' },
+      { name: 'regex', color: 'black' },
+      { name: 'keyword', color: 'black' },
+      { name: 'boolean', color: 'black' },
+      { name: 'function', color: 'black' },
+      { name: 'number', color: 'black' },
+      { name: 'operator', color: 'black' },
+      { name: 'ignore', color: 'black' },
+      { name: 'punctuation', color: 'black' }
     ]
+
+    var buildStyleRules = function( group ) {
+      return '.'
+      + group.name
+      + ' { color: '
+      + group.color
+      + ' }';
+    };
 
     $scope.groups = groups;
 
     $scope.styles = function() {
-      var i, group, numGroups = groups.length, style = '';
-
-      for( i=0; i<numGroups; i++ ) {
-        group = groups[i];
-        style += "." +group.name+" { color: "+group.color+" }\n";
-        // style += ".swatch-" +group.name+" { background-color: "+group.color+" }\n";
-      }
-
-      return style;
+      return groups.map( buildStyleRules ).join('\n');
     };
 
     $scope.$watch('groups', function() {
